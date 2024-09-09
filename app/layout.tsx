498e0a2365eb5d3bcd07e1323e0b1f28abe77b57
Farzad Khan <farzadk10000@gmail.com>
2024-09-09 12:12:43 +0530

    add @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FUT- | FAANG Ultimate Test",
-  description: "A game designed to Level up your coding skills with curated problems, on-the-spot feedback, and progress tracking. Each level presents a challenging problem commonly encountered in interviews at Google, Amazon, Facebook, and more. Solve each problem using our integrated code editor, receive immediate evaluation. Track your progress as you level up your skills.",
+  description:
+    "A game designed to Level up your coding skills with curated problems, on-the-spot feedback, and progress tracking. Each level presents a challenging problem commonly encountered in interviews at Google, Amazon, Facebook, and more. Solve each problem using our integrated code editor, receive immediate evaluation. Track your progress as you level up your skills.",
 };
 
 export default function RootLayout({
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
